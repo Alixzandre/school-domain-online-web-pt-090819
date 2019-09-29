3dc562f972a274b65484135430e018f6376939c5
Alexander Bispo <alexbispo173@gmail.com>
2019-09-29 19:09:45 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,5 +6,16 @@ class School
     @roster = {}
   end
   
+  def add_student(name, grade)
+    if @roster[grade] != nil
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
   
 end
